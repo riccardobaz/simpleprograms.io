3441ffd00e8ecdb5c32206b4d9371f78825761e9
riccardobaz <64643588+riccardobaz@users.noreply.github.com>
2026-01-04 19:14:09 +0100

    Update script3.js

--- script3.js
+++ script3.js
@@ -8,7 +8,7 @@
 
 const COLOR_PATTERNS = [
     { length: 11, color: 'lightgray', hex: '#D3D3D3' },
-    { length: 10, color: 'lime', hex: '#00FF66' },
+    { length: 10, color: 'darkgreen', hex: '#023d33' },
     { length: 9, color: 'green', hex: '#00FF00' },
     { length: 8, color: 'yellow', hex: '#ffff00' },
     { length: 7, color: 'purple', hex: '#9900ff' },
